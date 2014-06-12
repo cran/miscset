b609df41a4c5fe93fd1570506e99675bb0510ace
Sven E. Templer <sven.templer@gmail.com>
2014-06-12 11:29:00 +0000

    version 0.2

--- R/enpaire.R
+++ R/enpaire.R
@@ -1,21 +1,28 @@
-#' @name enpaire
-#' @keywords pairwise matrix
-#' @author Sven E. Templer (\email{sven.templer@@gmail.com})
-#' @title Create a Pairwise List from a Matrix
-#' @description
-#' Transform a matrix or dist object to a pairwise list.
+#' Create a Pairwise List from a Matrix
+#' 
+#' Transform a \code{matrix} or \code{dist} object to a pairwise list.
+#' 
 #' @param x Object of class \code{matrix}.
 #' @param upper Logical, return values from upper triangle.
 #' @param lower Logical, return values from lower triangle.
 #' @param ... Arguments passed to methods.
-#' @return Returns a \code{data.frame} with column 1 and 2 as row/column pair name,
-#' column 3 and 4 with upper and lower matrix values for each pair.
+#' @return
+#' Returns a \code{data.frame}. The first and second column represent the
+#' dimension names for a value in \code{x}. The following columns contain
+#' the values for the upper or lower triangle.
 #' @seealso
 #' \link{squarematrix}
 #' @examples
 #' #
+#' 
 #' m <- matrix(letters[1:9], 3, 3, dimnames = list(1:3,1:3))
 #' enpaire(m)
+#' enpaire(m, lower = FALSE)
+#' 
+#' #
+#' @name enpaire
+#' @keywords pairwise matrix
+#' @author Sven E. Templer (\email{sven.templer@@gmail.com})
 
 #' @rdname enpaire
 #' @export enpaire
@@ -23,26 +30,31 @@ enpaire <- function (x, ...) {
   UseMethod("enpaire")
 }
 
+#' @rdname enpaire
 #' @method enpaire default
-#' @S3method enpaire default
 enpaire.default <- function (x, ...) {
   stop("x must be of class matrix or dist.")
 }
 
 #' @rdname enpaire
+#' @export enpaire.dist
 #' @method enpaire dist
-#' @S3method enpaire dist
 enpaire.dist <- function (x, upper = T, lower = T, ...) {
   x <- as.matrix(x)
   NextMethod("enpaire")
 }
 
 #' @rdname enpaire
+#' @export enpaire.matrix
 #' @method enpaire matrix
-#' @S3method enpaire matrix
 enpaire.matrix <- function (x, upper = T, lower = T, ...) {
   if (!upper & !lower)
     stop("Nothing to return, set at least one of upper/lower TRUE.")
+  d <- dimnames(x)
+  if (is.null(d))
+    dimnames(x) <- list(1:nrow(x), 1:ncol(x))
+  else
+    dimnames(x) <- lapply(dimnames(x), function (y) if (is.null(y)) 1:nrow(x) else y)
   x <- squarematrix(x)
   pairs <- t(combn(rownames(x), 2))
   colnames(pairs) <- c("row", "col")
@@ -51,6 +63,6 @@ enpaire.matrix <- function (x, upper = T, lower = T, ...) {
     l <- t(x)[upper.tri(x)]
   if (lower)
     u <- t(x)[lower.tri(x)]
-  ret <- data.frame(pairs, upper = u, lower = l, stringsAsFactors = F)
+  ret <- data.frame(pairs, lower = l, upper = u, stringsAsFactors = F)
   return(ret)
 }
--- src/ntri.cpp
+++ src/ntri.cpp
@@ -1,28 +1,26 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-//' @name ntri
-//' @keywords triangle triangular number
-//' @author Sven E. Templer (\email{sven.templer@@gmail.com})
 //' @title Return Triangular Numbers
 //' @description
 //' Return the series of triangular (/triangle) numbers up to a number of
 //' \code{n} rows of a triangle. The series has the entry number "A000217"
-//' at \url{https://oeis.org/} and starts like this: 0, 1, 3, 6, 10, ...
-//' @param n Positive integer value for number of triangle rows.
+//' at \url{https://oeis.org/A000217} and starts like this:
+//' 0, 1, 3, 6, 10, ...
+//' @param n Positive integer value for sequence length.
 //' @return
 //' Returns an integer vector of length \code{n}.
+//' @name ntri
+//' @keywords triangle triangular number
 //' @import Rcpp
+//' @author Sven E. Templer (\email{sven.templer@@gmail.com})
+
 //' @export ntri
 // [[Rcpp::export]]
 NumericVector ntri(int n) {
-  
   NumericVector ret(n);
-  
   for (int i = 0; i < n; i ++) {
     ret[i] = i * (i + 1) / 2;
   }
-  
   return ret ;
-  
 }
